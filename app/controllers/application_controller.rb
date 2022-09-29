69f8b05c59a625d4b7daf799d5a31e48c46b0a99
Sam Higton <contact@samhigton.com>
2022-09-29 16:32:43 +0100

    add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Bas
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |email, password|
+      User.find_by(email: email)&.authenticate(password)
+    end
+  end
+
+  def current_user
+    @current_user ||= authenticate
+  end
 end
